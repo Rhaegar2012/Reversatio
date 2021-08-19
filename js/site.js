2a702024aa275173739da927e13d1fc1f2986720
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2021-08-18 20:12:30 -0700

    08182021-Excercise completed

--- js/site.js
+++ js/site.js
@@ -1,4 +1,16 @@
 //Call Hello World
-function HelloWorld(){
-    alert("hello world");
+function reverseString(){
+    let string=document.getElementById("textInput").value;
+    let stringReversed='';
+    for(let i=string.length-1; i>=0;i--){
+        stringReversed+=string[i];
+    }
+    displayResult(stringReversed);
+
+}
+function displayResult(text){
+    let jumbotronString=`<h4 class="display-4 header-dark">Well done!</h4>
+                         <hr class="my-4">
+                         <p>The reversed string is <strong>${text}<strong></p> `
+    document.getElementById("jumbotronOutput").innerHTML=jumbotronString;
 }
